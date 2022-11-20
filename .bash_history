f74b71c0562b4476370d2b1a924242490893e9c1
S Kota <91359399+Skota11@users.noreply.github.com>
2022-11-20 21:43:08 +0900

    Add files via upload

---
+++ .bash_history
@@ -0,0 +1,6 @@
+mkdir
+Mkdir new_directory
+mkdir new_directory
+mysql -u root
+myssql
+mysql
---
+++ project.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Skota11 Website-project</title>
+</head>
+<body>
+  <?php require("/app/common/header.php"); ?>
+	<main>
+		<h1 id="title2">Project</h1>
+    <p>
+    <h2>
+      Scratcherの通知数を確認しよう！！
+    </h2>
+    </p>
+    <h3>
+      <a href="https://sc-message.glitch.me">https://sc-message.glitch.me</a><br>このサイトで通知数を見よう！
+    </h3>
+    <h2>
+      Python,Scratchのプロジェクト公開
+    </h2>
+  <p>
+    Pythonのコードや、ScratchのプロジェクトをDropBoxで公開します。
+  </p>
+  <a href="https://www.dropbox.com/sh/7pfredtkgjg6uxd/AAANej1NhjL7OmpYduWxF9fya?dl=0">DropBoxのリンク（外部サイト）</a>
+    <P>
+    <h2>Scratcher's Advertising</h2>
+    </P>
+        <p><a href="https://scratch.mit.edu/users/tsumuri3/">tsumuri3</a>さんが作った広告載せます。</p>
+    <div id="iframe">
+		  <iframe src="https://scratcher-advertising.glitch.me/frame/frame-version.html" allowtransparency="true" width="300" height="250" style="border: 1px solid #ccc;" scrolling="no" allowfullscreen></iframe>
+      <iframe src="https://scratcher-advertising.glitch.me/frame/frame-version.html" allowtransparency="true" width="300" height="250" style="border: 1px solid #ccc;" scrolling="no" allowfullscreen></iframe>
+      <iframe src="https://scratcher-advertising.glitch.me/frame/frame-version.html" allowtransparency="true" width="300" height="250" style="border: 1px solid #ccc;" scrolling="no" allowfullscreen></iframe>
+    </div>
+	</main>
+  <footer>
+    <p id="copyright">
+      ©Skota11<br><a href="index.php">ホーム</a>
+    </p>
+  </footer>
+        <p id="page-top"><a href="#"><img src="https://cdn.glitch.me/d73516f0-bcdf-44ee-a2f0-8c5a3e5d0d82%2Fvertical_align_top_black_36dp.svg?v=1636185478123"></a></p>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+<script src="my.js"></script>
+</body>
+</html>
